18a224e98dc456cc30898f328a81da72ca8a56d9
Evillon1 <37180428+Evillon1@users.noreply.github.com>
2018-03-08 22:23:54 +0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,8 +4,9 @@ import random
 
 
 
-vk = vk_api.VkApi(login='+79517759822', password='XOXPzozp12345')
+#vk = vk_api.VkApi(login='+79517759822', password='XOXPzozp12345')
 #vk = vk_api.VkApi(login='+79681145585', password='1q2w3e4r')
+vk = vk.api.VkApi(token='548fc1039327bb25239228a067e5b235b4ebc483f050f06bda34d6863eecbba1bc966f2cd547e527c5316')
 #vk = vk_api.VkApi(token='9e46f0c3d3bd890ab68a85fc9b9a4d069ccfaa41f158f3f5c3b91214281c9fff8ad8b2518de66038d3722')
 vk._auth_token()
 values = {'out':0, 'coumt':100, 'time_offset':60}
